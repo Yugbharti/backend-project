75bba63602b2ee30d5770e8f77ab77132d996c3a
Yugbharti <yugbharti3@gamil.com>
2025-02-18 00:34:54 +0530

    utils 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { configDotenv } from "dotenv"
 import connectDB from "./db/index.js"
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is listening at ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Error in mongodb connection !! ",err);
+})
